1aad3589e2057cc997122107349a2463a814e254
Pranav Kumar <pranav.jenus@gmail.com>
2025-05-19 16:11:58 +0530

    Create P17_Socket_Server.cpp

---
+++ P17_Socket_Server.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+#include <winsock.h>
+
+#define PORT 9906
+using namespace std;
+
+int main()
+{
+    cout << "Socket Prog Creating Server Here" << endl;
+    int nRet = 0;
+    /// Initialize the WSA Variables
+    WSADATA ws;
+    if(WSAStartup(MAKEWORD(2,2),&ws)<0)
+    {
+     cout << " WSA Failed " << endl;
+    }
+    else
+    {
+        cout << "WSA Worked Fine" << endl;
+    }
+
+    /// Step 1: Initialize the socket
+    int nsocket = socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
+    // We can also have as below
+    // int nsocket = socket(AF_INET,SOCK_STREAM,0);
+    // Here passing 0 means use underlying Protocol
+
+    if(nsocket<0)
+    {
+        cout << "Failed to open Socket" << endl;
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        cout << "Socket Opened successfully"  << nsocket << endl;
+    }
+
+
+    /// Step 2: Initialize the environment for sock_addr structure
+    sockaddr_in srv;
+    srv.sin_family = AF_INET;
+    srv.sin_port = htons(9909);
+    srv.sin_addr.s_addr = INADDR_ANY; // Local Address of machine
+    //srv.sin_addr.s_addr = inet_addr("127.0.0.1");
+    memset(&(srv.sin_zero),0,8);
+
+
+    /// Step 3: Binding the socket to local port
+    nRet = bind(nsocket,(sockaddr*)&srv,sizeof(sockaddr));
+
+    if(nRet<0)
+    {
+        cout << "Binding Failed" << endl;
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        cout << "Binding Success" << endl;
+    }
+
+    /// Step 4: Listen the Request and define Request Queue Size
+    nRet = listen(nsocket,5);
+
+    if(nRet<0)
+    {
+        cout << "Listener Failed" << endl;
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        cout << "Listener Success" << endl;
+    }
+
+    /// Step 5: Keep waiting for new Request and proceed as per  the request
+
+    fd_set fr,fw,fe;
+    // fr file read
+    // fw  file write
+    // fe file exception, errors throws in network due to any failure
+    //These will be file descriptors on which we will write to the network, read over the nextwork
+    // AT max we can have 64 socket descriptors. Like there are 64 fr, 64, fw and 64 fe
+
+    int nmaxFd = nsocket;
+    struct timeval tv; // Specify timeout
+    tv.tv_sec = 1;
+    tv.tv_usec=0;
+
+
+
+    while(1)
+    {
+
+        // Set as Zero
+        FD_ZERO(&fr);
+        FD_ZERO(&fw);
+        FD_ZERO(&fe);
+
+        // Set the socket
+        FD_SET(nsocket,&fr);
+        FD_SET(nsocket,&fe);
+
+        cout << "Before select call fr.count=" << fr.fd_count << endl;
+
+        // select call un-sets all the file decriptor
+        nRet = select(nmaxFd+1,&fr,&fw,&fe,&tv);
+
+        if(nRet>0)
+        {
+            // When some one connects or communicates with Message
+            // over a dedicated connection
+        }
+        else if(nRet == 0)
+        {
+            // No connection or any communication which was made
+            // No-one of the sockets descriptors are ready
+            cout << "Nothing on the Port" << PORT << endl;
+        }
+        else
+        {
+            cout << "App failed" << endl;
+            // It failed and application has some error
+            exit(EXIT_FAILURE);
+        }
+        Sleep(2000);
+    }
+    cout << "After select call fr.count=" << fr.fd_count << endl;
+    return 0;
+}
