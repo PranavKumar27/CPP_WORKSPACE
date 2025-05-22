7e49bc1e627805809db79f1b81e9db0c975a25df
Pranav Kumar <pranav.jenus@gmail.com>
2025-05-22 14:02:30 +0530

    Update P22_Move_Constructor.cpp

--- P22_Move_Constructor.cpp
+++ P22_Move_Constructor.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -8,23 +9,45 @@ class Base
 public:
     Base()
     {
+        cout << "Default Constructor Called" << endl;
         val = nullptr;
     }
-    Base(int* v)
+    Base(int v)
     {
-        val = v;
+        val = new int;
+        *val = v;
+        cout << "Parameterized Constructor Called" << endl;
     }
-    Base(Base&& obj)
+    Base(Base&& obj) noexcept
     {
         val = obj.val;
-        obj.val = nullptr;
+
+        cout << "Move Constructor Called val=" << *val << endl;
+         //obj.val = nullptr;
+
     }
-    print()
+    Base(const Base& obj)
+    {
+        val = obj.val;
+        cout << "obj.val=" << *(obj.val) << endl;
+        cout << "Copy_Constructor Called" << endl;
+    }
+    void print()
     {
         if(val!=nullptr)
             cout << "val = " << *val << endl;
         else
             cout << "val = NULLPTR" << endl;
+    }
+    ~Base()
+    {
+        cout << "destructor called" << endl;
+        if(val!=nullptr)
+            cout << "val = " << *val << endl;
+        else
+            cout << "val = NULLPTR" << endl;
+
+        delete(val);
     }
 };
 
@@ -32,12 +55,22 @@ public:
 
 int main()
 {
-    cout << "Move Constructor" << endl;
+    cout << "$$$$$$$$$$$$$$   Move Constructor" << endl;
 
-    int v = 5;
-    Base obj1;
-    Base obj2(&v);
-    obj1.print();
-    obj2.print();
+
+   // int v = 5;
+    //Base obj1;
+    //Base obj2(v);
+    //Base obj3(Base{10});
+
+
+    vector<Base> vB;
+    vB.reserve(2);
+    vB.push_back(Base(10));
+    vB.push_back(Base{20});
+
+    vB.push_back(Base{30});
+    vB.push_back(Base{40});
+
     return 0;
 }
