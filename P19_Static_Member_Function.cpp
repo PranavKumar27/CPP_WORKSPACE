3b2fce153b6f99da2db0bb16bcd5b8ba89057d1f
Pranav Kumar <pranav.jenus@gmail.com>
2025-07-06 22:19:03 +0530

    Update P19_Static_Member_Function.cpp

--- P19_Static_Member_Function.cpp
+++ P19_Static_Member_Function.cpp
@@ -1,16 +1,22 @@
+
 #include <iostream>
 
 using namespace std;
 
 class Test
 {
-   static int x;
+   static int x;  // Make x as const to make it immutable for the class
+   //static const int x;
 public:
-    Test()
+    Test(int val)
     {
-        x = 0;
+        x = val;
     }
     static int  getX()
+    {
+        return x;
+    }
+    int getX_non_static()
     {
         return x;
     }
@@ -20,8 +26,22 @@ int Test::x=100;  // Must
 
 int main()
 {
-    Test t1;
-    cout << "X = " << Test::getX() << endl;
-    cout << "t1.X = " << t1.getX() << endl;
+    
+    cout << "X at start = " << Test::getX() << endl;
+    
+    Test t1(10);
+    //cout << "X = " << Test::x << endl;
+    cout << "X for Obj x1= " << Test::getX() << endl;
+    cout << "t1.X Static Method= " << t1.getX() << endl;
+    cout << "t1.X non static Method = " << t1.getX_non_static() << endl;
+    
+    Test t2(20);
+    //cout << "X = " << Test::x << endl;
+    cout << "X for obj x2= " << Test::getX() << endl;
+    cout << "t2.X Static Method= " << t2.getX() << endl;
+    cout << "t2.X non static Method = " << t2.getX_non_static() << endl;
+    
+    cout << "X at End = " << Test::getX() << endl;
+    
     return 0;
 }
