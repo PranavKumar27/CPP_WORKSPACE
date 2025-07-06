d8404f0640b447fa28f7b38452b8f233b3c51805
Pranav Kumar <pranav.jenus@gmail.com>
2025-07-06 23:00:33 +0530

    Update P21_Friend_Function.cpp

--- P21_Friend_Function.cpp
+++ P21_Friend_Function.cpp
@@ -18,55 +18,40 @@ public:
         Tejas_missile_key = "TJXXX25";
         Rafale_key = "@#$XXXX73K";
     }
-    friend class Israel;
-    friend bool check_with_Russia(India ind);
-    bool talk_to_India(Israel& talks)
-    {
-        cout << "India and Israel Jointly Make S500, S700 Defence Systems" << endl;
-    }
+    friend bool check_with_Russia();
 
 };
 
-bool check_with_Russia(India attack)
+bool check_with_Russia()
 {
+    India attack;
     cout << "India indegenious in Launching Tejas Key as " << attack.Tejas_missile_key  << " ------> "<< endl;
     cout << "India indegenious  in Launching Brahmos Key as " << attack.Brahmos_missile_key << " ------> " << endl;
     cout << "Russia help in Launching Rafael Key as " << attack.Rafale_key << " ------> " << endl;
+    cout << "Russia Drone Ultra Efficient Key as " << 249040 << " ------> " << endl;
     return true;
 }
-
-class Israel
-{
-    string S400_missile_key;
-public:
-    Israel()
-    {
-        S400_missile_key = "LXTYUIN77";
-    }
-    void Launch_All_Missile_to_Enemy(India attack)
-    {
-        cout << "Launching Tejas Key as " << attack.Tejas_missile_key  << " ------> "<< endl;
-        cout << "Launching Brahmos Key as " << attack.Brahmos_missile_key << " ------> " << endl;
-        cout << "Launching Rafael Key as " << attack.Rafale_key << " ------> " << endl;
-        cout << "Launching S400 Defence Key as " << S400_missile_key << " ------> " << endl;
-    }
-    friend bool talk_to_India(Israel& talks);
-};
-
-
 
 
 int main()
 {
     cout << "##################################  India Attacks Terrorism  ##################################  " << endl;
     India attack;
-    Israel Is;
-    Is.Launch_All_Missile_to_Enemy(attack);
+    
 
     cout << "##################################  Friend Function check with Russia ##################################  " << endl;
-    check_with_Russia(attack);
-    cout << "##################################  Friend Function check with Israel ##################################  " << endl;
-
-    attack.talk_to_India(Is);
+    check_with_Russia();
+    
     return 0;
 }
+
+/*
+
+##################################  India Attacks Terrorism  ##################################  
+##################################  Friend Function check with Russia ##################################  
+India indegenious in Launching Tejas Key as TJXXX25 ------> 
+India indegenious  in Launching Brahmos Key as MSXXXXRO20 ------> 
+Russia help in Launching Rafael Key as @#$XXXX73K ------> 
+Russia Drone Ultra Efficient Key as 249040 ------> 
+
+*/
